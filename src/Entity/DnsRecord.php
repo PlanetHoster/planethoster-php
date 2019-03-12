ba620ffa224ad3124a88fda8333b6211e119816c
Gabriel P <gabrielpt@support.planethoster.info>
2019-03-12 14:28:51 -0400

    DnsRecord entity

---
+++ src/Entity/DnsRecord.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace PlanetHoster\Entity;
+
+class DnsRecord extends Entity {
+
+  /**
+   * @var string
+   */
+  public $Hostname;
+
+  /**
+   * @var string
+   */
+  public $Address;
+
+  /**
+   * @var string
+   */
+  public $Type;
+
+  /**
+   * @var integer
+   */
+  public $Priority;
+}
