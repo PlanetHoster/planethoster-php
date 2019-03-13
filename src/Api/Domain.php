4f2c682bc9902876f642d7ff9bdbe9ecae9b01fe
Gabriel P <gabrielpt@support.planethoster.info>
2019-03-13 14:33:26 -0400

    add domain post methods

--- src/Api/Domain.php
+++ src/Api/Domain.php
@@ -3,6 +3,8 @@
 namespace PlanetHoster\Api;
 
 use PlanetHoster\Adapter\Adapter;
+use PlanetHoster\Entity\DnsRecord;
+use PlanetHoster\Entity\DomainContact;
 
 class Domain extends Api {
 
@@ -92,6 +94,206 @@ class Domain extends Api {
    */
   public function GetRegistrarLock($sld, $tld) {
     $content = $this->adapter->get($this->uri('get-registrar-lock'), [
+      'sld' => $sld,
+      'tld' => $tld,
+    ]);
+    return json_decode($content);
+  }
+
+  /**
+   * @param string        $sld
+   * @param string        $tld
+   * @param DomainContact $registrant
+   * @param DomainContact $admin
+   * @param DomainContact $tech
+   * @param DomainContact $billing
+   * 
+   * @return stdClass
+   */
+  public function SaveContactDetails($sld, $tld, DomainContact $registrant,
+      DomainContact $admin = null, 
+      DomainContact $tech = null, 
+      DomainContact $billing = null) {
+    $params = [
+      'sld' => $sld,
+      'tld' => $tld,
+    ];
+    $params = array_merge($params, $registrant->toArray('registrant'));
+    if($admin !== null) {
+      $params = array_merge($params, $admin->toArray('admin'));
+    }
+    if($tech !== null) {
+      $params = array_merge($params, $tech->toArray('tech'));
+    }
+    if($billing !== null) {
+      $params = array_merge($params, $billing->toArray('billing'));
+    }
+
+    $content = $this->adapter->post($this->uri('save-contact-details'), $params);
+    return json_decode($content);
+  }
+
+  /**
+   * @param string $sld
+   * @param string $tld
+   * @param array  $nameservers
+   * 
+   * @return stdClass
+   */
+  public function SaveNameservers($sld, $tld, $nameservers) {
+    $params = [
+      'sld' => $sld,
+      'tld' => $tld,
+    ];
+
+    for($i = 0; $i < sizeof($nameservers); $i++) {
+      $params[sprintf("ns%d", $i+1)] = $nameservers[$i];
+    }
+
+    $content = $this->adapter->post($this->uri('save-nameservers'), $params);
+    return json_decode($content);
+  }
+
+  /**
+   * @param string    $sld
+   * @param string    $tld
+   * @param DnsRecord $records
+   * 
+   * @return stdClass
+   */
+  public function SavePhDnsRecords($sld, $tld, DnsRecord ...$records) {
+    $params = [
+      'sld' => $sld,
+      'tld' => $tld,
+    ];
+
+    $count = 0;
+    foreach ($records as $record) {
+      $count++;
+      $params = array_merge($params, $record->toArray('', "$count"));
+    }
+
+    $content = $this->adapter->post($this->uri('save-ph-dns-records'), $params);
+    return json_decode($content);
+  }
+
+  /**
+   * @param string  $sld
+   * @param string  $tld
+   * @param boolean $lock
+   * 
+   * @return stdClass
+   */
+  public function SaveRegistrarLock($sld, $tld, boolean $lock) {
+    $content = $this->adapter->post($this->uri('save-registrar-lock'), [
+      'sld' => $sld,
+      'tld' => $tld,
+      'lock_action' => ($lock ? 'Lock' : 'Unlock'),
+    ]);
+    return json_decode($content);
+  }
+
+  /**
+   * @param string $sld
+   * @param string $tld
+   * 
+   * @return stdClass
+   */
+  public function EmailEppCode($sld, $tld) {
+    $content = $this->adapter->post($this->uri('email-epp-code'), [
+      'sld' => $sld,
+      'tld' => $tld,
+    ]);
+    return json_decode($content);
+  }
+
+  /**
+   * @param string        $sld
+   * @param string        $tld
+   * @param integer       $period
+   * @param array         $nameservers
+   * @param DomainContact $registrant
+   * @param DomainContact $admin
+   * @param DomainContact $tech
+   * @param DomainContact $billing
+   * @param boolean       $register_if_premium
+   * @param array         $addtl_fields
+   * 
+   * @return stdClass
+   */
+  public function RegisterDomain($sld, $tld, $period, $nameservers, DomainContact $registrant,
+      DomainContact $admin = null, 
+      DomainContact $tech = null, 
+      DomainContact $billing = null, 
+      $register_if_premium = false,
+      $addtl_fields = []) {
+    $params = [
+      'sld' => $sld,
+      'tld' => $tld,
+      'period' => $period,
+      'register_if_premium' => $register_if_premium,
+      'addtl_fields' => $addtl_fields,
+    ];
+
+    for($i = 0; $i < sizeof($nameservers); $i++) {
+      $params[sprintf("ns%d", $i+1)] = $nameservers[$i];
+    }
+
+    $params = array_merge($params, $registrant->toArray('registrant'));
+    if($admin !== null) {
+      $params = array_merge($params, $admin->toArray('admin'));
+    }
+    if($tech !== null) {
+      $params = array_merge($params, $tech->toArray('tech'));
+    }
+    if($billing !== null) {
+      $params = array_merge($params, $billing->toArray('billing'));
+    }
+
+    $content = $this->adapter->post($this->uri('register-domain'), $params);
+    return json_decode($content);
+  }
+
+  /**
+   * @param string  $sld
+   * @param string  $tld
+   * @param integer $period
+   * 
+   * @return stdClass
+   */
+  public function RenewDomain($sld, $tld, $period) {
+    $content = $this->adapter->post($this->uri('renew-domain'), [
+      'sld' => $sld,
+      'tld' => $tld,
+      'period' => $period,
+    ]);
+    return json_decode($content);
+  }
+
+  /**
+   * @param string $sld
+   * @param string $tld
+   * @param string $epp_code
+   * 
+   * @return stdClass
+   */
+  public function TransferDomain($sld, $tld, $epp_code) {
+    $content = $this->adapter->post($this->uri('transfer-domain'), [
+      'sld' => $sld,
+      'tld' => $tld,
+      'epp_code' => $epp_code,
+    ]);
+    return json_decode($content);
+  }
+
+  /**
+   * @param string $sld
+   * @param string $tld
+   * 
+   * @return stdClass
+   */
+  public function DeletePhDnsZone($sld, $tld) {
+    $content = $this->adapter->post($this->uri('delete-ph-dns-zone'), [
       'sld' => $sld,
       'tld' => $tld,
     ]);
