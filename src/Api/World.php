ba8869726b585558c28099ba8946f5d3fbf1b372
Gabriel P <gabrielpt@support.planethoster.info>
2019-03-13 14:43:16 -0400

    add world post calls

--- src/Api/World.php
+++ src/Api/World.php
@@ -15,6 +15,89 @@ class World extends Api {
   }
   
   /**
+   * @param string  $domain
+   * @param string  $country
+   * @param integer $cpu
+   * @param integer $mem
+   * @param integer $io
+   * @param string  $cms
+   * 
+   * @return stdClass
+   */
+  public function Create($domain, $country, $cpu, $mem, $io, $cms = '') {
+    $content = $this->adapter->post($this->uri('create-account'), [
+      'domain' => $domain,
+      'country' => $country,
+      'cpu' => $cpu,
+      'mem' => $mem,
+      'io' => $io,
+      'cms' => $cms,
+    ]);
+    return json_decode($content);
+  }
+
+  /**
+   * @param string  $id
+   * @param string  $reason
+   * 
+   * @return stdClass
+   */
+  public function Suspend($id, $reason) {
+    $content = $this->adapter->post($this->uri('suspend-account'), [
+      'id' => $id,
+      'reason' => $reason,
+    ]);
+    return json_decode($content);
+  }
+
+  /**
+   * @param string  $id
+   * 
+   * @return stdClass
+   */
+  public function Unsuspend($id) {
+    $content = $this->adapter->post($this->uri('unsuspend-account'), [
+      'id' => $id,
+    ]);
+    return json_decode($content);
+  }
+
+  /**
+   * @param string  $id
+   * @param integer $cpu
+   * @param integer $mem
+   * @param integer $io
+   * 
+   * @return stdClass
+   */
+  public function ModifyRessources($id, $cpu, $mem, $io) {
+    $content = $this->adapter->post($this->uri('modify-ressources'), [
+      'id' => $id,
+      'cpu' => $cpu,
+      'mem' => $mem,
+      'io' => $io,
+    ]);
+    return json_decode($content);
+  }
+  
+
+  /**
+   * @param integer $cpu
+   * @param integer $mem
+   * @param integer $io
+   * 
+   * @return stdClass
+   */
+  public function UpgradePlan($cpu, $mem, $io) {
+    $content = $this->adapter->post($this->uri('upgrade-plan'), [
+      'cpu' => $cpu,
+      'mem' => $mem,
+      'io' => $io,
+    ]);
+    return json_decode($content);
+  }
+
+  /**
    * @param string $path
    * 
    * @return string
