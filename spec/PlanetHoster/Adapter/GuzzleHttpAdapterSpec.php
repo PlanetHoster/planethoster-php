731caeccb4fb5a51eb755bb7c43139c12b47e82d
Gabriel P <gabrielpt@support.planethoster.info>
2019-03-14 13:15:21 -0400

    GuzzleHttpAdapter tests

--- spec/PlanetHoster/Adapter/GuzzleHttpAdapterSpec.php
+++ spec/PlanetHoster/Adapter/GuzzleHttpAdapterSpec.php
@@ -3,6 +3,8 @@
 namespace spec\PlanetHoster\Adapter;
 
 use GuzzleHttp\Client;
+use GuzzleHttp\Psr7\Response;
+use GuzzleHttp\Psr7\Stream;
 use PlanetHoster\Adapter\GuzzleHttpAdapter;
 use PhpSpec\ObjectBehavior;
 use Prophecy\Argument;
@@ -25,5 +27,41 @@ class GuzzleHttpAdapterSpec extends ObjectBehavior
     function it_is_initializable()
     {
         $this->shouldHaveType(GuzzleHttpAdapter::class);
+    }
+
+    function it_should_return_json(Client $client, Response $response, Stream $stream) 
+    {
+      $client->request('GET', '/json', ['query' => ''])->willReturn($response);
+      $response->getBody()->willReturn($stream);
+      $stream->__toString()->willReturn('{"foo":"bar"}');
+
+      $this->get('/json')->shouldBe('{"foo":"bar"}');
+    }
+
+    function it_should_set_query_params(Client $client, Response $response, Stream $stream)
+    {
+      $client->request('GET', '/query', ['query' => ['foo'=>'bar']])->willReturn($response);
+      $response->getBody()->willReturn($stream);
+      $stream->__toString()->willReturn('{"foo":"bar"}');
+
+      $this->get('/query',['foo'=>'bar'])->shouldBe('{"foo":"bar"}');
+    }
+
+    function it_should_post_params(Client $client, Response $response, Stream $stream)
+    {
+      $client->request('POST', '/post', ['form_params' => ['foo'=>'bar']])->willReturn($response);
+      $response->getBody()->willReturn($stream);
+      $stream->__toString()->willReturn('{"foo":"bar"}');
+
+      $this->post('/post',['foo'=>'bar'])->shouldBe('{"foo":"bar"}');
+    }
+
+    function it_should_post_content(Client $client, Response $response, Stream $stream)
+    {
+      $client->request('POST', '/postcontent', ['body' => 'foo=bar'])->willReturn($response);
+      $response->getBody()->willReturn($stream);
+      $stream->__toString()->willReturn('{"foo":"bar"}');
+
+      $this->post('/postcontent','foo=bar')->shouldBe('{"foo":"bar"}');
     }
 }
