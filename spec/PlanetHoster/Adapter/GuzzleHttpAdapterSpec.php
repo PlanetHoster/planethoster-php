0cb78b8c4722deec4a6f54ed6dce01aa0d2e137d
Normand lemay <lem.norm@gmail.com>
2022-01-28 10:44:34 -0500

    Update GuzzleHttpAdapterSpec.php
    Adding new headers params

--- spec/PlanetHoster/Adapter/GuzzleHttpAdapterSpec.php
+++ spec/PlanetHoster/Adapter/GuzzleHttpAdapterSpec.php
@@ -17,11 +17,12 @@ class GuzzleHttpAdapterSpec extends ObjectBehavior
             'headers' => [
               'X-API-USER' => 'api_user',
               'X-API-KEY' => 'api_key',
+              'X-API-SANDBOX' => 'api_sandbox',
             ],
             'timeout' => 10,
             'base_uri' => 'http://dev.null'
           ]);
-        $this->beConstructedWith('api_user', 'api_key', 10, 'http://dev.null', $client);
+        $this->beConstructedWith('api_user', 'api_key', 'api_sandbox', 10, 'http://dev.null', $client);
     }
 
     function it_is_initializable()
