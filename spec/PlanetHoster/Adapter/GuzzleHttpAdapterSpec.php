f0d478c90f1a94d5fb081a85b9453fbd1f1c8f80
Gabriel P <gabriel.pt@planethoster.info>
2022-09-23 12:15:07 -0400

    fix: let test

--- spec/PlanetHoster/Adapter/GuzzleHttpAdapterSpec.php
+++ spec/PlanetHoster/Adapter/GuzzleHttpAdapterSpec.php
@@ -13,15 +13,6 @@ class GuzzleHttpAdapterSpec extends ObjectBehavior
 {
     function let(Client $client)
     {
-        $client->beConstructedWith([
-            'headers' => [
-              'X-API-USER' => 'api_user',
-              'X-API-KEY' => 'api_key',
-              'X-API-SANDBOX' => 'api_sandbox',
-            ],
-            'timeout' => 10,
-            'base_uri' => 'http://dev.null'
-          ]);
         $this->beConstructedWith('api_user', 'api_key', 'api_sandbox', 10, 'http://dev.null', $client);
     }
 
